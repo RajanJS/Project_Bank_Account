5c6a99efe8a1720ca26cb9f61304fb62c3791691
RajanJS <rajanmaharjanmails@gmail.com>
2016-02-23 09:22:42 +0545

    completed

--- bank-account.js
+++ bank-account.js
@@ -1,13 +1,7 @@
+//account array to store accounts information
 var accounts = [];
 
-//Account Object
-//Balance
-//username
-
-
-//createAccount(account)
-//push onto Account array
-//return account
+//function to create an account
 function createAccount(account){
 	accounts.push(account);	
 	return account;	
@@ -18,49 +12,60 @@ function createAccount(account){
 //find matching account using forEach
 function getAccount(username){
 	var matchedAccount;
-	accounts.forEach(function(account){
-		if(account.username === username){
-			matchedAccount = account;
-		}
-	});
 
+	for(var i = 0; i<accounts.length ; i++){
+		if(accounts[i].username === username){
+			matchedAccount = accounts[i];
+		}
+	}
 	return matchedAccount; 
 }
 
 
 //deposit
 	function deposit(acc,amt){
+		if(typeof amt === 'number'){
 		acc.balance += amt;
+		console.log('You have successfully deposited '+amt+ ' in '+ acc.username+'\'s Account.');
+		}else{
+			console.log('Coundn\'t deposit not an valid number !');
+		}
 	}
 //withdraw
 	function withdraw(acc,amt){
-		acc.balance -= amt;
+		if(typeof amt === 'number'){
+			acc.balance -= amt;
+		console.log('You have successfully withdrawn '+amt+ ' from '+ acc.username+'\'s Account.');
+		}else{
+			console.log('Coundn\'t withdraw not an valid number !');
+		}
 	}
+
 //getBalance
 	function getBalance(acc){
 		return acc.balance;
 	}
 
+
+
+function createBalanceGetter (acc){
+	return function(){
+		return acc.balance;
+	}
+}
+
+
+
+//Create Account
 var rajansAccount = createAccount({
 	username: 'Rajan',
 	balance: 0
 });
 
 deposit(rajansAccount,10000000000);
-console.log(getBalance(rajansAccount));
 
 withdraw(rajansAccount,5000);
-console.log(getBalance(rajansAccount));
 
-var existingAccount1 = getAccount('Rajan');
-console.log(getBalance(existingAccount1));
-
-var julsAccount = createAccount({
-	username: 'Juls',
-	balance: 90000000000
-});
-
-console.log(accounts);
-
-var existingAccount2 = getAccount('Juls');
-console.log(existingAccount2);
+var rajan1 = getAccount('Rajan');
+var getRajanBalance  = createBalanceGetter(rajan1);
+console.log('Total remainig balance in '+rajan1.username+' \'s account is '+getRajanBalance());
